d4a0cfce457c20389989467d16c643fc91e2242d
ec0zhang <1871496693@qq.com>
2019-05-14 23:15:52 +0800

    first commit

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -2,6 +2,23 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  var myArray = [];
+  if (number_a === number_b) {
+    myArray = [number_a]
+  }
+  else if (number_a > number_b) {
+    for (let i = number_b; i <= number_a; i++) {
+      myArray[i - 1] = i;
+    }
+    myArray.reverse();
+  }
+  else if (number_a < number_b) {
+    for (let i = number_a; i <= number_b; i++) {
+      myArray[i - 1] = i;
+    }
+  }
+
+  return myArray;
 }
 
 module.exports = get_integer_interval;

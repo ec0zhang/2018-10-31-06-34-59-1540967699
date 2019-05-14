6cce51aaf9caa538e1560c5abd2f58936214b9f5
ec0zhang <34810442+ec0zhang@users.noreply.github.com>
2019-05-14 23:23:42 +0800

    Update collect_all_even.js
    first change

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -2,6 +2,15 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  // collection = [2, 1, 3, 4, 5];
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] % 2 === 1) {
+      collection.splice(i, 1);
+      i--;//保证每一个数字都遍历到
+    }
+  }
+  return collection;
+  // collection.filter(collections => collections%2 === 0);
 }
 
 module.exports = collect_all_even;

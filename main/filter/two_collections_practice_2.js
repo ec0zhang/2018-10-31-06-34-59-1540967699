42888499f062eca95ec52b27a7b41dd0fcfbd17f
ec0zhang <34810442+ec0zhang@users.noreply.github.com>
2019-05-15 23:22:28 +0800

    Update two_collections_practice_2.js

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -3,6 +3,14 @@
 function choose_no_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+  let myArray1 = collection_a.filter(function (v) {
+    return collection_b.indexOf(v) === -1;
+  });
+  let myArray2 = collection_b.filter(function (v) {
+    return collection_a.indexOf(v) === -1;
+  });
+
+  return myArray1.concat(myArray2);
 }
 
 module.exports = choose_no_common_elements;

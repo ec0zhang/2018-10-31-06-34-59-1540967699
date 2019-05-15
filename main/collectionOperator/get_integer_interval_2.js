6bf97fb46e8ca362a256ecc9b0b94867ebc3c694
ec0zhang <34810442+ec0zhang@users.noreply.github.com>
2019-05-15 23:16:18 +0800

    Update get_integer_interval_2.js

--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,25 @@
 'use strict';
 
-function get_integer_interval_2(number_a, number_b) {
+function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  var myArray = [];
+  if (number_a === number_b) {
+    myArray = [number_a];
+  }
+  else if (number_a > number_b) {
+    for (let i = number_b; i <= number_a; i++) {
+      myArray[i - 1] = i;
+    }
+    myArray.reverse();
+  }
+  else if (number_a < number_b) {
+    for (let i = number_a; i <= number_b; i++) {
+      myArray[i - 1] = i;
+    }
+  }
+
+  return myArray;
 }
 
-module.exports = get_integer_interval_2;
+module.exports = get_integer_interval;
+

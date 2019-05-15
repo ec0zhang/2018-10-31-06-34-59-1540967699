19cfb534880b095df7602e4cbcc5268e1ac33b8d
ec0zhang <34810442+ec0zhang@users.noreply.github.com>
2019-05-15 23:17:49 +0800

    Update get_integer_interval_2.js

--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -1,25 +1,35 @@
 'use strict';
 
-function get_integer_interval(number_a, number_b) {
+function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
   var myArray = [];
   if (number_a === number_b) {
-    myArray = [number_a];
-  }
-  else if (number_a > number_b) {
-    for (let i = number_b; i <= number_a; i++) {
-      myArray[i - 1] = i;
-    }
-    myArray.reverse();
+    if (number_a % 2 === 0)  myArray = [number_a];
+    // else myArray = [];
   }
   else if (number_a < number_b) {
-    for (let i = number_a; i <= number_b; i++) {
+    for (let i = number_a;i <= number_b ;i++){
       myArray[i - 1] = i;
     }
+    for (let i = 0;i < myArray.length;i++){
+      if (myArray[i] %2 === 1){
+        myArray.splice(i,1);
+      }
+    }
+  }
+  else if (number_a > number_b){
+    for (let i = number_b;i <= number_a ;i++){
+      myArray[i - 1] = i;
+    }
+    for (let i = 0;i < myArray.length;i++){
+      if (myArray[i] %2 === 1){
+        myArray.splice(i,1);
+      }
+    }
+    myArray.reverse();
   }
 
   return myArray;
 }
 
-module.exports = get_integer_interval;
-
+module.exports = get_integer_interval_2;

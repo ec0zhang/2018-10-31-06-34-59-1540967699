b3b9b913a0a087a95abb5495bb985d3bdde0dffe
ec0zhang <34810442+ec0zhang@users.noreply.github.com>
2019-05-15 23:23:20 +0800

    Update two_collections_practice_3.js

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -3,6 +3,11 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  return collection_a.filter(function (v) {
+    return v % 2 === 0 || v % 3 === 0 || v % 5 === 0;
+  });
+
+  // return myArray1;
 }
 
 module.exports = choose_divisible_integer;

94b127888f98f9166fd378e5685d3e1bdd1463eb
ec0zhang <1871496693@qq.com>
2019-06-18 22:20:57 +0800

    18

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function double_to_one(collection) {
-
   //在这里写入代码
+  return [].concat.apply([], collection);
+  // apply的一个巧妙的用处,可以将一个数组默认的转换为一个参数列表([param1,param2,param3] 转换为 param1,param2,param3)
 }
 
 module.exports = double_to_one;
--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,15 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var result = [].concat.apply([], collection);
+  var temp = [];
+  for (let i = 0; i <= result.length - 1; i++) {
+    if (temp.indexOf(result[i]) === -1) {
+      temp.push(result[i]);
+    }
+  }
+
+  return temp;
 }
 
 module.exports = double_to_one;
